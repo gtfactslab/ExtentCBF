2c8bfd3911373a265c13783fd079e62513e83334
Mohit Srinivasan <mohit.srinivasan@gmail.com>
2020-03-30 14:18:35 -0400

    Delete circle.m

--- SOS_Controller/circle.m
+++
@@ -1,9 +0,0 @@
-function h = circle(x,y,r)
-hold on
-th = 0:pi/50:2*pi;
-xunit = r * cos(th) + x;
-yunit = r * sin(th) + y;
-h = plot(xunit, yunit,'Color',[0.72, 0.506, 0.125], ...
-                      'LineWidth', 2, ...
-                       'HandleVisibility', 'off');
-hold off
